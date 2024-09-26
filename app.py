cade17136668cb2ce0b57184085fbb6dfe1d32c8
Anirudhgvs <anirudhgvs@pepsales.co>
2024-09-26 21:43:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Pepsales!!'
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0')
