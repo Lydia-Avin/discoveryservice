7dfc2d0509cea897776f077e96c07f5a3721da14
Lydia-Avin <lydiamary@pepsales.co>
2024-10-07 16:24:05 +0530

    main.py added

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def root():
+        return jsonify(message="Welcome to Pepsales!")
+
+    return app
--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Pepsales Design Team!!'
 
 if __name__ == '__main__':
     app.run(port=8000, host='0.0.0.0')
