fafc7af50ba07bc221d9d2c986c7024afebea7ce
Lydia-Avin <lydiamary@pepsales.co>
2024-10-10 09:25:19 +0530

    test

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,6 @@ def create_app():
 
     @app.route("/")
     def root():
-        return jsonify(message="Welcome to Pepsales!! 1 ")
+        return jsonify(message="Welcome to Pepsales!! 2 ")
 
     return app
