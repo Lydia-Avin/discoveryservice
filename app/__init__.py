d2e879b8c432fb5818151d657508f8ba01e76541
Lydia-Avin <lydiamary@pepsales.co>
2024-10-28 16:08:43 +0530

    secrets updated

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,6 @@ def create_app():
 
     @app.route("/")
     def root():
-        return jsonify(message="Welcome to Pepsales! ")
+        return jsonify(message="Welcome to Pepsales :) ! ")
 
     return app
