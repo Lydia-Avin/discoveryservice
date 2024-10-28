ba2b70772da26e4c5f38b9eea980fadb642d4afc
Lydia-Avin <lydiamary@pepsales.co>
2024-10-28 19:53:09 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -6,5 +7,9 @@ def create_app():
     @app.route("/")
     def root():
         return jsonify(message="Welcome to Pepsales!!!")
+    
+    @app.route("/env")
+    def env():
+        return jsonify(message=os.environ.get("AWS_ACCESS_KEY_ID"))
 
     return app
